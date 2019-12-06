be54c7d20a98cddf7ca9ff6430ae489ca9504ed2
Katha <suhr.katharina@gmail.com>
2019-12-06 20:57:27 +0100

    first try 6.12.

---
+++ FAZ.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'faz_article'
+    start_urls = [
+        'https://www.faz.net/aktuell/politik/ausland/',
+    ]
+
+    def parse(self, response):
+        link_selector = '//div[contains(@class, "ctn-List")]//a[contains(@class, "ContentLink")]/@href'
+        next_page_selector = '//li[contains(@class, "next-page")]/a/@href'
+
+        for faz_article in response.xpath(link_selector).getall():
+            yield {'url': faz_article}
+
+        next_page = response.xpath(next_page_selector).get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+#scrapy runspider quotes_spider.py -o quotes.json
---
+++ FAZ_index.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class FAZIndexSpider(CrawlSpider):
+    name = 'faz_index'
+    allowed_domains = ['faz.net']
+    start_urls = [
+        'https://www.faz.net/aktuell/',
+    ]
+
+    rules = (
+        Rule(LinkExtractor(allow=('/aktuell/.*'))),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        item = scrapy.Item()
+        item['author'] = response.xpath('//div[contains(@class, "atc-Meta")]//span[@class="atc-MetaAuthor"]/text()').getall()
+        item['text'] = response.xpath('//div[contains(@class, "Text")]//p[contains(@class, "TextParagraph")]/text()').getall()
+        return item
+#link_selector = '//div[contains(@class, "Articles")]//a[contains(@class, "lbl-Base") and not(contains(@class, "lbl-Base-has-icon"))]/@href'
+#scrapy runspider quotes_spider.py -o quotes.json
