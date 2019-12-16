054d0c591916f0197045cc32a508a1ba260009c6
Tristan <s5trdiet@uni-trier.de>
2019-12-16 23:51:46 +0100

    parse function for heise. gets the urls but parsing of the articles has yet to be implemented.

--- cpt/cpt/spiders/heise.py
+++ cpt/cpt/spiders/heise.py
@@ -5,7 +5,20 @@ import scrapy
 class HeiseSpider(scrapy.Spider):
     name = 'heise'
     allowed_domains = ['heise.de']
-    start_urls = ['https://www.heise.de/newsticker/it']
+    start_urls = ['https://www.heise.de/newsticker/it/']
 
     def parse(self, response):
-        url = response
+        article_xpath = "//article/a/@href"
+
+        for url in response.xpath(article_xpath).getall():
+            yield {
+                scrapy.Request(url, callback=self.parse_article)
+            }
+
+        next_page = response.css('li.a-pagination__item').xpath('@href').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_article(self, response):
+        pass
