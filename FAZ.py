c4a200f556f976cad251240bd070240029434ee6
Katha <suhr.katharina@gmail.com>
2019-12-16 16:20:39 +0100

    try

--- FAZ.py
+++ FAZ.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class FAZSpider(scrapy.Spider):
@@ -9,13 +10,35 @@ class FAZSpider(scrapy.Spider):
 
     def parse(self, response):
         link_selector = '//div[contains(@class, "ctn-List")]//a[contains(@class, "ContentLink")]/@href'
-        next_page_selector = '//li[contains(@class, "next-page")]/a/@href'
+        #next_page_selector = '//li[contains(@class, "next-page")]/a/@href'
 
-        for faz_article in response.xpath(link_selector).getall():
-            yield {'url': faz_article}
+        for faz_article in response.xpath(link_selector).get():
+            self.logger.info('Parse function called on %s', response.url)
+            yield response.follow(faz_article, self.parse_article)
 
-        next_page = response.xpath(next_page_selector).get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+    def parse_article(self, response):
+        title = response.xpath('//span[@class="atc-HeadlineEmphasisText"]/text()').get()
+        subtitle = response.xpath('//span[@class="atc-HeadlineText"]/text()').get()
+        author = response.xpath('//a[@class="atc-MetaAuthorLink"]/text()').get()
+        article_source = response.xpath('//span[@class="atc-Footer_Quelle"]/text()').get()
+
+        JSON = response.xpath('//script[contains(@type, "json")]').get()
+
+        yield {
+            #'title': title + ": " + subtitle,
+            #'author': str(author) + "(" + str(article_source) + ")",
+            'json': JSON
+        }
+
+
+        #article = response.url.split("/")[-1]
+        #filename = 'article-%s' % article
+        #with open(filename, 'wb') as f:
+        #    f.write(response.body)
+        #self.log('Saved file %s' % filename)
+
+        #next_page = response.xpath(next_page_selector).get()
+        #if next_page is not None:
+        #    yield response.follow(next_page, self.parse)
 
 #scrapy runspider quotes_spider.py -o quotes.json
--- FAZ_index.py
+++ FAZ_index.py
@@ -6,8 +6,7 @@ Politik = "https://www.faz.net/aktuell/politik"
 Wirtschaft = "https://www.faz.net/aktuell/wirtschaft/"
 Finanzen = "https://www.faz.net/aktuell/finanzen/"
 Sport = "https://www.faz.net/aktuell/sport/"
-Kultur = "https://www.faz.net/aktuell/feuilleton/"
-Kultur2 = "https://www.faz.net/aktuell/stil/"
+Kultur = ["https://www.faz.net/aktuell/feuilleton/", "https://www.faz.net/aktuell/stil/"]
 Gesellschaft = "https://www.faz.net/aktuell/gesellschaft/"
 Reisen = "https://www.faz.net/aktuell/reise/"
 Technik = "https://www.faz.net/aktuell/technik-motor/"
@@ -21,7 +20,7 @@ Karriere = "https://www.faz.net/aktuell/karriere-hochschule/"
 class FAZSpider(scrapy.Spider):
     name = 'faz_index'
     start_urls = [
-        Politik
+        Technik
         #, Wirtschaft, Finanzen, Sport, Kultur, Kultur2, Gesellschaft, Reisen, Technik, Wissen, Regional, Karriere
     ]
 
@@ -29,9 +28,9 @@ class FAZSpider(scrapy.Spider):
         selector_subcategories = "//div[contains(@class, 'Articles')]//a[contains(@class, 'is-link') and starts-with(@href, '/aktuell')]/@href"
 
         for faz_index in response.xpath(selector_subcategories).getall():
-            yield response.follow(faz_index, self.parse_article)
+            yield response.follow(faz_index, self.parse_index)
 
-    def parse_article(self, response):
+    def parse_index(self, response):
 
         selector_articles = '//div[contains(@class, "ctn-List")]//a[contains(@class, "ContentLink")]/@href'
         #Fplus = "//span[contains(@class, 'Headline') and not(contains(@class, 'has-icon') or contains(@class, 'Text'))]"
@@ -39,12 +38,21 @@ class FAZSpider(scrapy.Spider):
 
         for faz_article in response.xpath(selector_articles).getall():
             self.logger.info('Parse function called on %s', response.url)
-            yield {'url': faz_article}
+            yield response.follow(faz_article, self.parse_article)
 
         next_page = response.xpath(next_page_selector).get()
         self.logger.info('next_page %s', next_page)
         if next_page is not None:
-            yield response.follow(next_page, self.parse_article)
+            yield response.follow(next_page, self.parse_index)
+
+    def parse_article(self, response):
+        article = response.url.split("/")[-1]
+        filename = 'article-%s' % article
+        with open (filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+
+
 
 #//div[contains(@class, 'Lead')]//a[contains(@class, 'is-link')]/@href
 #//li[contains(@class, 'TopicsListItem')]/a[contains(@href, 'aktuell')]/@href
