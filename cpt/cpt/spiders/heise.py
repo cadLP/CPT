cd3f77d9b91908a23b4b4f4cf71e9b499976a456
Tristan <s5trdiet@uni-trier.de>
2020-01-06 09:50:20 +0100

    Gets articles and next pages. Implemented filter for newsticker articles.

--- cpt/cpt/spiders/heise.py
+++ cpt/cpt/spiders/heise.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
+import re
 import scrapy
 
 
 class HeiseSpider(scrapy.Spider):
     name = 'heise'
-    allowed_domains = ['heise.de']
+    allowed_domains = ['www.heise.de']
     start_urls = ['https://www.heise.de/newsticker/it/']
 
     def parse(self, response):
-        article_xpath = "//article/a/@href"
+        article_xpath = "//a[@class='a-article-teaser__link']/@href"
+        article_url_regex = re.compile(r'/newsticker/meldung/.+?')
 
         for url in response.xpath(article_xpath).getall():
-            yield {
-                scrapy.Request(url, callback=self.parse_article)
-            }
+            if re.match(article_url_regex, url):
+                # print(url)
+                article_url = response.urljoin(url)
+                yield scrapy.Request(article_url, callback=self.parse_article)
 
-        next_page = response.css('li.a-pagination__item').xpath('@href').get()
+        next_page = response.xpath('//li[has-class("a-pagination__item--next")]/a/@href').get()
+        # print(next_page)
+        next_page = response.urljoin(next_page)
+
         if next_page is not None:
-            next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_article(self, response):
-        pass
+        print("Parsing...")
