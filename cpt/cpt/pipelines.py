368dc68aea6a7613072b85e253cb737b8f709176
Tristan <s5trdiet@uni-trier.de>
2020-01-28 09:51:04 +0100

    Started to implement pipline.

--- cpt/cpt/pipelines.py
+++ cpt/cpt/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import psycopg2
 
 
 class CptPipeline(object):
+
+    def open_spider(self, spider):
+        hostname = 'localhost'
+        username = 'crawler'
+        password = ''
+        database = 'crawler'
+        self.conneciton = psycopg2.connect(host=hostname, user=username,
+                                           password=password, database=database)
+        self.cur = self.conneciton.cursor()
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conneciton.close()
+
     def process_item(self, item, spider):
         return item
