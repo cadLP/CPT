d62a256639750294d9608999e6361e10976aa413
Katha <suhr.katharina@gmail.com>
2020-01-30 18:13:54 +0100

    article extraction

--- FAZ/FAZ/pipelines.py
+++ FAZ/FAZ/pipelines.py
@@ -21,21 +21,35 @@ class FazPipeline(object):
         self.cur = self.conn.cursor()
 
     def process_item(self, item, spider):
-        self.store_db(item)
+        self.store_db_metadata(item)
+        self.store_db_text(item)
         return item
 
-    def store_db(self, item):
+    def store_db_metadata(self, item):
         self.cur.execute(
-            """Insert into metadaten (title, date_retrieved, date_published, date_edited, url, language, keywords, media, id, authors, category, source)
-            values (%s, current_date, %s, %s, %s, %s, %s, %s, default, %s, %s, 'FAZ')""", (
-                item['title'],
-                item["date_published"],
-                item["date_edited"],
-                item["url"],
-                item["language"],
-                item["keywords"],
-                item["media"],
-                item["author"],
-                item["category"]
-            ))
+            """INSERT INTO metadaten (title, date_retrieved, date_published, date_edited, 
+            url, language, keywords, media, id, authors, category, source)
+            VALUES ('{title}', current_date, '{pub}', '{edit}', '{url}', '{lang}', 
+            '{keyw}', '{media}', default, '{author}', '{cate}', 'FAZ');""".format(
+                title=item['title'],
+                pub=item["date_published"],
+                edit=item["date_edited"],
+                url=item["url"],
+                lang=item["language"],
+                keyw=item["keywords"],
+                media=item["media"],
+                author=item["author"],
+                cate=item["category"]
+            )
+        )
+        self.conn.commit()
+
+    def store_db_text(self, item):
+        self.cur.execute(
+            """INSERT INTO text (article_text, metadaten_id)
+            VALUES ('{text}', (SELECT id FROM metadaten WHERE url='{url}'));""".format(
+                text=item["article_text"],
+                url=item["url"]
+            )
+         )
         self.conn.commit()
